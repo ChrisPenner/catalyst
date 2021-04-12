c19a46c79f517ade5db31e8d64514f9c9102fa50
Chris Penner <christopher.penner@gmail.com>
2021-04-11 21:33:15 -0600

    Hide numeric and closed for now

--- src/Control/Category/Closed.hs
+++ src/Control/Category/Closed.hs
@@ -1,9 +1,8 @@
 module Control.Category.Closed where
 
-import Control.Category (Category, (>>>), (<<<))
 
-class Category k => Closed k where
-  -- closed :: (a `k` b) -> ((r -> a) `k` (r -> b))
-  apply :: ((a -> b), a) `k` b
-  curry' :: ((a, b) `k` c) -> (a `k` (b -> c))
-  uncurry' :: (a `k` (b -> c)) -> ((a, b) `k` c)
+-- class Category k => Closed k where
+--   -- closed :: (a `k` b) -> ((r -> a) `k` (r -> b))
+--   apply :: ((a -> b), a) `k` b
+--   curry' :: ((a, b) `k` c) -> (a `k` (b -> c))
+--   uncurry' :: (a `k` (b -> c)) -> ((a, b) `k` c)
